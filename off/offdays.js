463b4335e98b48e003595c30412b710ac6fabf5f
Brad <bradharris@harrismbrad.com>
2022-12-22 06:08:55 -0800

    added Map, and vars for service locations

--- off/offdays.js
+++ off/offdays.js
@@ -2,7 +2,7 @@
 // They also may include days that most others do not do routine work or tasks. It could be a culturial resonon why. 
 import { Friday, offDays } from './friday/day';
 
-offDays(Saturday, Sunday) = new reviewDays;
+offDays(Saturday, Sunday) = new reviewDays; // Review days may need to be defined with a monthly loop
 
 
 
