f3135b4feec242ef68ec5135accaf41c94157214
Brad <bradharris@harrismbrad.com>
2022-12-24 06:36:57 -0800

    updated to include holydays, removed Friday offday

--- friday/day.js
+++ friday/day.js
@@ -1,22 +1,10 @@
 import { done } from './day/off/offdays';
 import { Work } from './day/Thursday/day';
 
-done = undefined;
-Friday(`12-23-22`);
-people = wantTo;
-something = good;
-somethingGood === good;
-Work != wantTo;
-console.log(something);
+offDays = [Saturday, Sunday];
+onDays = done(- Saturday - Sunday);
+WorkWeek[Monday, Tuesday, Wednesday, Thursday, Friday] = onDays;
 
-(Friday = offDay)
-console.log(wantTo);
-people === wantTo
-console.log(somethingGood);
-
-
-const offDays = [Saturday, Sunday];
-const onDays = done(- Saturday - Sunday);
-const [Monday, Tuesday, Wednesday, Thursday, Friday] = onDays;
+WorkWeek = [];
 
 export default done; 
