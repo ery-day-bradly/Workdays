e726abd47a81db0ba162aebd7d61cfc6b559feb4
Brad <bradharris@harrismbrad.com>
2022-12-27 06:33:36 -0800

    added Flow, Feature, and new TODO for dev work

--- tuesday/day.js
+++ tuesday/day.js
@@ -1,3 +1,28 @@
-import { Monday } from './days/tuesday/day';
+Flow = new flow(feature);
+feature(flowing);
+Today = new today(flow);
+DailyStandup = new meetings(flow);
+Huddles = new huddles(flow);
+// Huddles = StandUps;
+// Work = Flow; 
+flowing(Monday, Today); {
+  //
+  console.log('feature-flow', TODO);
+}
+/**Today
+ * What is flow? How do we know we are flowing?
+ * A: attended two or more meetings.
+ * A: You are flowing if you are able to quickly start QA Testing needs, or start more writing on code...
+ * Start working on Issues assignment discussed from Meetings/ huddles.
+ * > Flow TODO: Continue QA Testing Needs...
+ * > Flow TODO> Curation of Jira backlogs while QA Testing Continues...
+ * Remember, Flow is the feature...
+ */
 
-export default Monday;
+/**day
+ * Yesterday
+ * > Flow TODO: Continue QA Testing Needs...
+ * > Flow TODO> Curation of Jira backlogs while QA Testing Continues...
+ * > What did you discover?
+ */
+//export default Monday;
