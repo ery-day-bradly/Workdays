e974b8c304162893452854ff2a79269d01ca48ad
Brad <bradharris@harrismbrad.com>
2023-01-20 06:04:56 -0800

    btn libs for new app screen, wip

--- panelday/src/App.js
+++ panelday/src/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 import './App.css';
 
 
+
 // This feature has the user make three clicks in a row to see welcome. 
 export default function App() {
   return (
-    <div>
-      <body>
-        <button onUploadImage={() => alert('like-button-root')} />
-        <main id="like-button-root">  {(<button onClick={alert("Welcome")}>Welcome</button>)}  </main>
-        <script src="https://unpkg.com/react@18/umd/react.development.js" crossOrigin></script>
-        <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossOrigin></script>
-        <script src="../off/likeBtn.js"></script>
-      </body>
-    </div>
+
+    <body>
+      <button onUploadImage={() => alert('like-button-root')} />
+      <main id="like-button-root">  {(<button onClick={alert("Welcome")}>Welcome</button>)}
+
+
+
+
+
+      </main>
+      <script src="https://unpkg.com/react@18/umd/react.development.js" crossOrigin></script>
+      <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossOrigin></script>
+      <script src="../off/likeBtn.js"></script>
+    </body>
+
   );
 }
 
