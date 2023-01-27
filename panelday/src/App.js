137ef4b7091f58e3950b96eca51625559ecbc961
Brad <bradharris@harrismbrad.com>
2023-01-27 06:11:03 -0800

    added form and canvas elements WIP, might not keep

--- panelday/src/App.js
+++ panelday/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './App.css';
 
+let canvas = document.getElementById("myWelcomeCanvas");
+let ctx = canvas.getContext("like-button-root");
 
+ctx("myWelcomeCanvas");
 
 // This feature has the user make three clicks in a row to see welcome. 
 export default function App() {
   return (
 
     <body>
-      <button onUploadImage={ () => alert('like-button-root') } />
-      <main id="like-button-root">  { (<button onClick={ alert("Welcome") }>Welcome</button>) }
+      <button onUploadImage={() => alert('like-button-root')} />
+      <>
+        <main id="like-button-root">  {(<button onClick={alert("myWelcomeCanvas")}>Welcome</button>)}
+          <form>
+            <label for="submit-button">Submit Button:</label>
+            <button id="submit-button" type="submit">Submit</button>
 
+            <label for="reset-button">Reset Button:</label>
+            <button id="reset-button" type="reset">Reset</button>
 
+            <label for="custom-button">Custom Button:</label>
+            <button id="custom-button" type="button">Click me!</button>
+          </form>
+        </main>
+      </>
 
-
-
-      </main>
       <script src="https://unpkg.com/react@18/umd/react.development.js" crossOrigin></script>
       <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossOrigin></script>
       <script src="../off/likeBtn.js"></script>
@@ -23,11 +34,4 @@ export default function App() {
 
   );
 }
-
-// const styles = StyleSheet.create({
-//   logo: { padding: 1 },
-//   box: { padding: 8 },
-//   text: { fontWeight: 'noopener noreferrer' }
-// });
-
 
